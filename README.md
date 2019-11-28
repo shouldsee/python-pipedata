21a74ccbea2dc0f3156fd743dbc550250c7571a0
Travis CI <travis@travis-ci.org>
2019-11-28 23:41:53 +0000

    add requirements

---
+++ README.md
@@ -0,0 +1,16 @@
+## pipedata
+
+A lightweight package that version controls pipeline and data.
+
+### Installation
+
+```bash
+pip2 install pipedata@https://github.com/shouldsee/python-pipedata/tarball/master --user
+pip2 install https://github.com/shouldsee/python-pipedata/tarball/master --user
+```
+
+### Test
+
+```bash
+python2 test.py
+```
---
+++ setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+#from setuptools import setup
+from distutils.core import setup
+#import setuptools
+import os,glob,sys
+os.chdir(os.path.dirname(__file__))
+setup(
+	name='pipedata',
+	version='0.0.1',
+	packages=[
+	'.'
+#        'delay_and_cache',
+             ],
+	include_package_data=True,
+	license='MIT',
+	author='Feng Geng',
+	author_email='shouldsee.gem@gmail.com',
+	long_description=open('README.md').read(),
+	install_requires=[
+		x.strip() for x in open("requirements.txt","r")
+        	if x.strip() and not x.strip().startswith("#")
+	],
+
+)
+
+
