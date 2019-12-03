7101297075bdb689470199d1f4ff9b96921577c8
shouldsee <shouldsee@github.com>
2019-12-03 16:21:32 +0000

    [archive][clean]

--- tests/example_stage2.py
+++ tests/example_stage2.py
@@ -9,24 +9,6 @@ index = IndexNode()
 # _d = TrackedDict(index,'meta', 
 #     dict(DATA_ACC="ABC123"))
 
-_ = '''
-if remote out5 is triggered, its index will be put into queue but not flushed.
-There are two choices here: 
-  1. leave the upstream in a non-synced state, by caching a unsync flag into the influenced nodes, 
-    - for TrackedFile, this has to override the default
-    - The default backbone in a level_stream is RawNode. It should take all its output with it,
-    if the funcion opens a TrackedFile and modified it, it does not differ if the TrackedFile
-    index_update() itself or whether its triggered by the main_node(), because the timestamp 
-    is the same.
-    - however, for TrackedDict() the result of as_record() depends on whether the main_node has
-    been executed. One solution would be dump the dictionary into a TrackedFile. Because the 
-    data comes from the execution of the main_node(), it would not be possible to predict it
-    unless the main_node is executed, 
-
-  2. The easier option would be to update the upstream to be in sync.
-  - in order to test for this, alter out5.txt, run the downstream script and check the status of combined.txt
-  
-'''
 RemoteNode(index, remote_path='../stage1/pipe.py',remote_name='out5')
 
 @MasterNode.from_func(index,)
